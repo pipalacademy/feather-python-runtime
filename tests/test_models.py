5ea2a98fdcb733ac832a0a7d7f3166211f4c0f1d
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-08-03 12:23:50 +0530

    Add tests for when multiple files were submitted

--- tests/test_models.py
+++ tests/test_models.py
@@ -25,6 +25,36 @@ def test_get_runrequest_from_flask_request_with_multipart(app):
         assert file.stream.read().decode("utf-8") == content
 
 
+def test_get_runrequest_from_flask_request_with_multipart_and_multiple_files(
+    app,
+):
+    test_files = {
+        "test.py": "Hello, world!",
+        "test2.py": "content of test2.py",
+        "test3.txt": "content of test3.txt",
+    }
+
+    with app.test_request_context(
+        "/runtimes/python",
+        method="POST",
+        data={
+            filename: (BytesIO(content.encode("utf-8")), filename)
+            for filename, content in test_files.items()
+        },
+    ):
+        run_request = RunRequest.from_request(request)
+
+        assert run_request.code is None
+        assert run_request.mode == RunRequestMode.FILES
+        assert len(run_request.files) == 3
+
+        for filename in test_files:
+            file = run_request.files[filename]
+            expected_content = test_files[filename]
+            assert file.filename == filename
+            assert file.stream.read().decode("utf-8") == expected_content
+
+
 def test_get_runrequest_from_flask_request_with_json(app):
     filename = "test.py"
     content = """print("Hello from inside a test, world!")"""
@@ -43,6 +73,35 @@ def test_get_runrequest_from_flask_request_with_json(app):
         file = run_request.files[filename]
         assert file.filename == filename
         assert file.stream.read().decode("utf-8") == content
+
+
+def test_get_runrequest_from_flask_request_with_json_and_multiple_files(app):
+    test_files = {
+        "test.py": "Hello, world!",
+        "test2.py": "content of test2.py",
+        "test3.txt": "content of test3.txt",
+    }
+
+    with app.test_request_context(
+        "/runtimes/python",
+        method="POST",
+        json={
+            "files": {
+                filename: content for filename, content in test_files.items()
+            }
+        },
+    ):
+        run_request = RunRequest.from_request(request)
+
+        assert run_request.code is None
+        assert run_request.mode == RunRequestMode.FILES
+        assert len(run_request.files) == 3
+
+        for filename in test_files:
+            file = run_request.files[filename]
+            expected_content = test_files[filename]
+            assert file.filename == filename
+            assert file.stream.read().decode("utf-8") == expected_content
 
 
 def test_get_runrequest_from_flask_request_with_code(app):
