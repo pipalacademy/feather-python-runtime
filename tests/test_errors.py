a7adab10520d5150aa32565f86e1a7d5c7d517c5
Kaustubh Maske Patil <37668193+nikochiko@users.noreply.github.com>
2022-08-05 12:28:52 +0530

    #10: Add tests for errors

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,10 +1,76 @@
+from tests.conftest import get_run_endpoint
+
+
+endpoint = get_run_endpoint()
+
+
 def test_unsupported_content_type(client):
     response = client.post(
-        "/runtimes/python",
-        headers={"Content-Type": "unsupported/type"},
+        endpoint,
+        headers={"content-type": "unsupported/type"},
         data="something random",
     )
 
     assert response.status_code == 415
     assert "error" in response.json
     assert "message" in response.json
+    assert response.json["error"] == "Unsupported content type"
+
+
+def test_incorrect_json_error(client, run_endpoint):
+    json_data = {"main.py": "this main.py should be under a 'files' key"}
+
+    response = client.post(
+        endpoint,
+        json=json_data,
+    )
+
+    assert response.status_code == 400
+    assert "error" in response.json
+    assert "message" in response.json
+    assert response.json["error"] == "Invalid input"
+
+
+def test_code_not_found_error_with_form_urlencoded(client):
+    response = client.post(endpoint, data="")
+
+    assert response.status_code == 400
+    assert "error" in response.json
+    assert "message" in response.json
+    assert response.json["error"] == "Invalid input"
+
+
+def test_code_not_found_error_with_textplain(client):
+    response = client.post(
+        endpoint, headers={"Content-Type": "text/python"}, data=""
+    )
+
+    assert response.status_code == 400
+    assert "error" in response.json
+    assert "message" in response.json
+    assert response.json["error"] == "Invalid input"
+
+
+def test_code_not_found_error_with_json(client):
+    response = client.post(
+        endpoint,
+        json={},
+    )
+
+    assert response.status_code == 400
+    assert "error" in response.json
+    assert "message" in response.json
+    assert response.json["error"] == "Invalid input"
+
+
+def test_code_not_found_error_with_multipart(client):
+    response = client.post(
+        endpoint,
+        headers={"Content-Type": "multipart/form-data"},
+        data={},
+    )
+
+    assert response.status_code == 400
+    assert "error" in response.json
+    assert "message" in response.json
+    assert response.json["error"] == "Invalid input"
